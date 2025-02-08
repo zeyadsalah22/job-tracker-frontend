a835766a0c7d2741755cf6569cef1cc953cd431e
aliaaabobakr12 <aliaaabobakr12@gmail.com>
2025-02-08 22:44:27 +0200

    Update axios base URL to use production server

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect } from "react";
 
-const BASE_URL = "http://127.0.0.1:8000/api";
+const BASE_URL = "https://better-fiann-zeyadsalah-3825b5df.koyeb.app/api";
 
 export default axios.create({
   baseURL: BASE_URL,
